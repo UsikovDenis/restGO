b757e1f945a80436afac26f0cec8315e91222a2d
denis <denis.usikov95@mail.ru>
2025-06-21 11:45:05 +0300

    go-clean-architecture

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,17 @@ import (
 	"log"
 	"restGO"
 	"restGO/pkg/handler"
+	"restGO/pkg/repository"
+	"restGO/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
